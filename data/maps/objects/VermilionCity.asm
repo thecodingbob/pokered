395585ac5f2fc9c58fb357f53a189b6d447108fd
thecodingbob <vey90@live.it>
2022-09-16 17:52:20 +0200

    cleaning up vermilion city

--- data/maps/objects/VermilionCity.asm
+++ data/maps/objects/VermilionCity.asm
@@ -9,16 +9,16 @@ VermilionCity_Object:
 	warp_event  7,  3, SS_ANNE_2F, 7
 
 	def_bg_events
-	bg_event 29,  3,  7 ; VermilionCityText7
-	bg_event 16,  3,  9 ; MartSignText
-	bg_event 12,  3, 10 ; PokeCenterSignText
-	bg_event  5,  3, 11 ; VermilionCityText11
-	bg_event 21,  3, 12 ; VermilionCityText12
+	bg_event 29,  3,  1 ; VermilionCityText1
+	bg_event 16,  3,  2 ; MartSignText
+	bg_event 12,  3,  3 ; PokeCenterSignText
+	bg_event  5,  3,  4 ; VermilionCityText2
+	bg_event 21,  3,  5 ; VermilionCityText3
 
 	def_object_events
 	object_event 35,  7, SPRITE_BEAUTY, STAY, LEFT, 1 ; person
 	object_event 35, 10, SPRITE_GAMBLER, STAY, LEFT, 2 ; person
-	object_event 39, 35, SPRITE_SAILOR, STAY, UP, 3 ; person
+	object_event 30, 14, SPRITE_SAILOR, STAY, UP, 3 ; person
 	object_event 21, 10, SPRITE_GAMBLER, STAY, UP, 4 ; person
 	object_event 19, 10, SPRITE_MONSTER, STAY, UP, 5 ; person
 	object_event 35,  4, SPRITE_SAILOR, STAY, LEFT, 6 ; person
