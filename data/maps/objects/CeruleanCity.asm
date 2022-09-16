d51c5161515773697148190ecbe98f9ee6543d46
thecodingbob <vey90@live.it>
2022-09-16 11:38:45 +0200

    added watching npcs in cerulean & route 24

--- data/maps/objects/CeruleanCity.asm
+++ data/maps/objects/CeruleanCity.asm
@@ -23,15 +23,15 @@ CeruleanCity_Object:
 
 	def_object_events
 	object_event 20,  2, SPRITE_BLUE, STAY, DOWN, 1 ; person
-	object_event 29, 21, SPRITE_ROCKET, STAY, NONE, 2, OPP_ROCKET, 5
-	object_event 13, 16, SPRITE_COOLTRAINER_M, STAY, DOWN, 3 ; person
-	object_event 15, 18, SPRITE_SUPER_NERD, WALK, UP_DOWN, 4 ; person
-	object_event  9, 21, SPRITE_SUPER_NERD, WALK, LEFT_RIGHT, 5 ; person
+	object_event 23, 15, SPRITE_ROCKET, STAY, UP, 2, OPP_ROCKET, 5
+	object_event  5, 16, SPRITE_COOLTRAINER_M, STAY, UP, 3 ; person
+	object_event  7, 16, SPRITE_SUPER_NERD, STAY, UP, 4 ; person
+	object_event 26, 15, SPRITE_SUPER_NERD, STAY ,UP, 5 ; person
 	object_event 17, 12, SPRITE_GUARD, STAY, DOWN, 6 ; person
-	object_event 29, 26, SPRITE_COOLTRAINER_F, STAY, LEFT, 7 ; person
-	object_event 28, 26, SPRITE_MONSTER, STAY, DOWN, 8 ; person
-	object_event  9, 27, SPRITE_COOLTRAINER_F, WALK, LEFT_RIGHT, 9 ; person
-	object_event 39, 31, SPRITE_SUPER_NERD, STAY, DOWN, 10 ; person
-	object_event 19, 22, SPRITE_GUARD, STAY, DOWN, 11 ; person
+	object_event 33, 13, SPRITE_COOLTRAINER_F, STAY, LEFT, 7 ; person
+	object_event 29, 15, SPRITE_MONSTER, STAY, UP, 8 ; person
+	object_event 11, 16, SPRITE_COOLTRAINER_F, STAY, UP, 9 ; person
+	object_event 23,  7, SPRITE_SUPER_NERD, STAY, LEFT, 10 ; person
+	object_event  9, 16, SPRITE_GUARD, STAY, UP, 11 ; person
 
 	def_warps_to CERULEAN_CITY
